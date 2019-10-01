e8e1af3e74939895df2226e64f7626503ecd0c4c
Gloria Moraga <silmeriel@gmail.com>
2019-10-01 18:26:16 -0300

    for and if vue

--- 01.js
+++ 01.js
@@ -1,6 +1,12 @@
 const app = new Vue ({
   el: '#app',
   data:{
-    greeting: 'Hola Vue'
+    titulo: 'Hola Vue',
+    // frutas:['Manzana', 'Pera', 'Platano']
+    frutas:[
+      {nombre:'Manzana', cantidad: 10},
+      {nombre:'Pera', cantidad: 0},
+      {nombre:'Platano',cantidad: 6}
+    ]
   }
 })
