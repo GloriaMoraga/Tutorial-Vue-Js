7bf30feb752c2d198f0371a12c03547c0a1f75eb
Gloria Moraga <silmeriel@gmail.com>
2019-11-21 17:49:24 -0300

    tercer capitulo

--- 01.js
+++ 01.js
@@ -7,6 +7,31 @@ const app = new Vue ({
       {nombre:'Manzana', cantidad: 10},
       {nombre:'Pera', cantidad: 0},
       {nombre:'Platano',cantidad: 6}
-    ]
+    ],
+    nuevaFruta:'',
+    total: 0
+
+  },
+  methods:{
+    agregarFruta (){
+     this.frutas.push({
+       nombre:this.nuevaFruta, 
+       cantidad: 0
+
+     });
+     this.nuevaFruta = ''
+
+    }
+  },
+  computed:{
+    sumarFrutas(){
+      this.total = 0;
+      for(fruta of this.frutas){
+        this.total = this.total + fruta.cantidad;
+      }
+      return this.total;
+
+    }
   }
+
 })
